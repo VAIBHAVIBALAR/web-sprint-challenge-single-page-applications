a4c6f53cf9843150769906b317270cd7a9bee389
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-23 16:45:37 -0700

    final cypress commit

--- cypress/integration/sprinttest.js
+++ cypress/integration/sprinttest.js
@@ -3,8 +3,15 @@ describe('Lambda Eats', ()=>{
     cy.visit('http://localhost:3000')
 })
 it('check for right elements showing', ()=>{
+    //for textbox
     textInput().should('exist')
+
+    //for submit button
     submitBtn().should('exist')
+
+    //for topping
+    cy.get('input[type="checkbox"]')
+      .check(['onions','echeese','pepperoni']);
 })
 })
 
